0d186cc591e52af61b51364704e2a92b0b9f8ea4
Stefan Wiese <stefan.wiese@camunda.com>
2020-12-18 08:59:50 +0100

    removeEmployeeFromHRSystem

--- camunda.onboarding/src/main/java/camunda/onboarding/workflow/adapter/RemoveUserFromHRSystemDelegate.java
+++ camunda.onboarding/src/main/java/camunda/onboarding/workflow/adapter/RemoveUserFromHRSystemDelegate.java
@@ -1,18 +1,15 @@
 package camunda.onboarding.workflow.adapter;
 
-import java.util.Date;
-
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import camunda.onboarding.workflow.ProcessConstants;
-import camunda.onboarding.workflow.entity.Employee;
 import camunda.onboarding.workflow.repository.EmployeeRepository;
 
-@Component("addEmployeeToHRSystem")
-public class AddEmployeeToHRSystemDelegate implements JavaDelegate {
+@Component("removeUserFromHRSystem")
+public class RemoveUserFromHRSystemDelegate implements JavaDelegate {
 
 	@Autowired
 	private EmployeeRepository employeeRepository;
@@ -20,8 +17,8 @@ public class AddEmployeeToHRSystemDelegate implements JavaDelegate {
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
 
-		Employee employee = employeeRepository.save(new Employee("Maxe", "Meier", "8.5", "L", "Boss", "HR", new Date(), "emp-1"));
-		execution.setVariable(ProcessConstants.EMPLOYEE_ID, employee.getId());
+		Long employeeId = (Long) execution.getVariable(ProcessConstants.EMPLOYEE_ID);
+		employeeRepository.deleteById(employeeId);
 		employeeRepository.flush();
 	}
 
